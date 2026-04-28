caf938598fd7e292fafb00466fdad821afab4dc8
Drayage <111415523+Drayage@users.noreply.github.com>
2026-04-28 18:25:58 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'dorast-v1';
+const urlsToCache = [
+  '/DoraST/',
+  '/DoraST/index.html',
+  '/DoraST/island_escape/game.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
