771a85299abe695ed12fb65f85b80754898cea95
Drayage <111415523+Drayage@users.noreply.github.com>
2026-04-28 12:00:43 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'mangrove-v0.2';
+
+// 언더바 반영된 경로 확인해봐, 언니!
+const ASSETS = [
+  './',
+  './index.html',
+  './island_escape/game.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('파일 캐싱 중... 시발 노가다 성공! 📦');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
